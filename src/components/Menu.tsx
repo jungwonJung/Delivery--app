d993f973919fc223b927b942d3272869593cb2fd
unknown <wjdwjd1501@gmail.com>
2023-11-08 12:09:08 +0100

    file structure setup

---
+++ src/components/Menu.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Menu() {
+  return (
+    <div>
+        <Image src='/open.png' alt='' width={20} height={20}/>
+    </div>
+  )
+}
+
+export default Menu
